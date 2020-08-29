1c991c7af53e1554ba5d729ee67011a2d0b35fdb
full-stack-coder <ken.boyer@outlook.com>
2020-08-29 14:06:42 +0000

    clean up /dist directory

--- src/dist/index.dev.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-
-var _reactDom = _interopRequireDefault(require("react-dom"));
-
-var _App = _interopRequireDefault(require("./App"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
